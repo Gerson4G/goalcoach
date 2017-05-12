62a18abf67b87fcbf68e8300e094827ba90f0f9c
Gerson4G <gersongoatache@gmail.com>
2017-05-12 00:39:22 -0400

    Pre-Redux

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBqfAxB5OXlqe4OQl7-ZnWk4UrmAok7Mok",
+    authDomain: "goalcoach-b5261.firebaseapp.com",
+    databaseURL: "https://goalcoach-b5261.firebaseio.com",
+    projectId: "goalcoach-b5261",
+    storageBucket: "goalcoach-b5261.appspot.com",
+    messagingSenderId: "417083348228"
+  };
+
+export const firebaseApp = firebase.initializeApp(config);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+import {firebaseApp} from './firebase'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 import './index.css';
+import { Router, Route, browserHistory } from 'react-router';
+
+firebaseApp.auth().onAuthStateChanged(user => {
+	if(user){
+		console.log('user has signed in or up', user);
+	} else{
+		console.log('User needs to sign in');
+	}
+})
 
 ReactDOM.render(
-  <App />,
+	<Router path="/" history={browserHistory}>
+  		<Route path="/app" component={App} />
+  		<Route path="/signin" component={SignIn} />
+  		<Route path="/signup" component={SignUp} />
+	</Router>,
   document.getElementById('root')
 );
